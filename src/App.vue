56e128d9f4ee33494185cf9febecbd85dbd3f1ae
arnoliudaxia <liu1456447215@gmail.com>
2023-06-26 11:48:49 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Lottie from './components/Lottie.vue';
+import a1 from './lottiejson/home.json';
+
+export default {
+  data() {
+    return {
+      jsonData: a1,
+    };
+  },
+  components: {
+    Lottie,
+  },
+};
+</script>
+
+<template>
+  <!-- 使用本地json动画 -->
+  <Lottie :jsonData="jsonData" />
+  <!-- 使用线上json动画 -->
+  <Lottie src="https://assets10.lottiefiles.com/packages/lf20_dyiqnus5.json" />
+</template>
